9f3d7ad322bc44e1808e3899c987b8b52f6985e1
surajsherchan <41652804+surajsherchan@users.noreply.github.com>
2024-01-21 15:09:34 -0800

    Add Button functionality App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Deployment test</h1>
       <p>Check the changes</p>
+      <button>Click me</button>
+      <button onClick={() => console.log('Button clicked!')}>Click me!</button>
     </div>
   );
 }
